b8994284ba361b75cf58530a2bc0584d4aee4b0e
Orla Madden <omadden2@gmail.com>
2020-07-31 06:14:52 +0000

    added shopping bag app and urls

---
+++ bag/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+
+# Create your views here.
+def view_bag(request):
+    """ A view to return shopping bag """
+    return render(request, 'bag/bag.html')
