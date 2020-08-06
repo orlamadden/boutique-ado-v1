72b168e977780fbdd2ccf8323a458996957a8eff
Orla Madden <omadden2@gmail.com>
2020-08-06 07:05:23 +0000

    completed checkout stage

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,4 @@
+  
 import uuid
 
 from django.db import models
@@ -6,9 +7,8 @@ from django.conf import settings
 
 from products.models import Product
 
-# Create your models here.
-class Order(models.Model):
 
+class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
@@ -23,6 +23,8 @@ class Order(models.Model):
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    original_bag = models.TextField(null=False, blank=False, default='')
+    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
 
     def _generate_order_number(self):
         """
